8c800d4beb27f99389f5ffc9e45cb9b1bb35a24d
Israel Alves <108505958+Khaldewey@users.noreply.github.com>
2023-05-19 19:15:35 +0000

    atualização5

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,30 +1,30 @@
 export const dataFake = [
   {
-    "id":1,
-    "photo":"aaa",
-    "title": "saiu novo filme",
-    "description":"ola"
+    "id":"1",
+    "title": "Filme que vai fazer sucesso",
+    "description":"A Marvel vai surpreender",
+    "photoCover":"https://legadodamarvel.com.br/wp-content/uploads/2023/05/Cena-de-Homem-de-Ferro-proibida-pela-Marvel-e-finalmente-revelada-legadodamarvel.webp"
 
   }, 
   {
-    "id":2,
-    "photo":"aaa",
-    "title": "saiu novo filme",
-    "description":"ola"
+    "id":"2",
+    "title": "Guerra Ultimato fez todo esse sucesso mesmo?",
+    "description":"ola",
+    "photoCover":"https://br.web.img3.acsta.net/newsv7/19/04/22/20/34/5550692.jpg"
 
   },
   {
-    "id":3,
-    "photo":"aaa",
-    "title": "saiu novo filme",
-    "description":"ola"
+    "id":"3",
+    "title": "Saudades dos bons filmes da marvel",
+    "description":"ola",
+    "photoCover":"https://tm.ibxk.com.br/2022/05/04/04181551056727.jpg?ims=1200x675"
 
   },
   {
-    "id":4,
-    "photo":"aaa",
-    "title": "saiu novo filme",
-    "description":"ola"
+    "id":"4",
+    "title": "Capitão Améééééérica",
+    "description":"ola", 
+    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2022/04/Sam-Wilson-Capitao-America.jpg"
 
   }
 ]
--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { dataFake } from 'src/app/data/dataFake';
 
 @Component({
   selector: 'app-content',
@@ -12,11 +13,25 @@ photoCover:string = ""
 @Input()
 contentTitle:string = ""
 @Input()
-contentDescription:string = ""
+contentDescription:string = "" 
+private id:string | null = "0"
 constructor(
   private route:ActivatedRoute
 ){}
 ngOnInit(): void{
-  this.route.paramMap.subscribe(value => console.log(value.get("id")))
+  this.route.paramMap.subscribe(value => 
+    this.id = value.get("id"))
+} 
+
+setValuesToComponent(id:string | null){
+ const result = dataFake.filter(
+  article => article.id.toString() == id )[0] 
+  
+  this.contentTitle = result.title 
+  this.contentDescription = result.description
+  this.photoCover =  result.photoCover
+  
+ 
 }
+
 }
