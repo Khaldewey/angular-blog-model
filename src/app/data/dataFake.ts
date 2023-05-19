efe95f500e9955d0e22c170fd8713e2d8cbea28b
Israel Alves <108505958+Khaldewey@users.noreply.github.com>
2023-05-19 17:47:44 +0000

    testando mv

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -5,6 +5,27 @@ export const dataFake = [
     "title": "saiu novo filme",
     "description":"ola"
 
+  }, 
+  {
+    "id":2,
+    "photo":"aaa",
+    "title": "saiu novo filme",
+    "description":"ola"
+
+  },
+  {
+    "id":3,
+    "photo":"aaa",
+    "title": "saiu novo filme",
+    "description":"ola"
+
+  },
+  {
+    "id":4,
+    "photo":"aaa",
+    "title": "saiu novo filme",
+    "description":"ola"
+
   }
 ]
 
